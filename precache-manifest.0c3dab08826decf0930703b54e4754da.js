20a0f08c3ea8c4b43ccc8062f15d8f09718e3851
Lucie Rabahi <lucierabahi@protonmail.com>
2020-01-04 09:19:50 +0100

    save portfolio

---
+++ precache-manifest.0c3dab08826decf0930703b54e4754da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "891596255fc218d59157d969bf8cdd8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ebf7a459c46e6f1894d",
+    "url": "/static/css/main.fb38dbf3.chunk.css"
+  },
+  {
+    "revision": "14bb9ee80d5af0253e6d",
+    "url": "/static/js/2.a5b82793.chunk.js"
+  },
+  {
+    "revision": "4ebf7a459c46e6f1894d",
+    "url": "/static/js/main.4a1b9c2a.chunk.js"
+  },
+  {
+    "revision": "9f3b6f5d85035e0b1d9d",
+    "url": "/static/js/runtime-main.a45aa8df.js"
+  }
+]);
